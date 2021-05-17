5ba7df4199d6d3e8297040ee926ed8abadf0cfdb
Danilo Cupido <danilocupido@Danilos-MacBook-Air.local>
2021-05-17 17:04:32 +0100

    content and style update remove sidebar, content box3 and anchor tags text-decoration. Add dark mode button

--- script.js
+++ script.js
@@ -1,5 +1,16 @@
 const intro = document.querySelector("h1");
+const ligthDark = document.querySelector("#ligth_dark");
+const main = document.querySelector("main");
 
 intro.addEventListener("click", () => {
-  intro.textContent = "You are here";
+  intro.textContent = "Welcome!";
+}); //this is an easter egg!
+
+ligthDark.addEventListener("click", () => {
+  main.classList.toggle("darkBackground");
+  if (ligthDark.textContent === "Ligth to Dark") {
+    ligthDark.textContent = "Dark to Ligth";
+  } else {
+    ligthDark.textContent = "Ligth to Dark";
+  }
 });
